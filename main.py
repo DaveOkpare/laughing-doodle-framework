d853f19225174dea64c04970bea9608c919f1b0e
DaveOkpare <okparedave@gmail.com>
2023-01-14 16:33:45 +0100

    Added typing datatypes to __call__ specifications

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+from typing import Dict, Callable, Any
+
+
 class API:
-    def __call__(self, environ, start_response):
+    def __call__(self, environ: Dict, start_response: Callable[..., Any]):
         response_body = b"Hello World"
         status = "200"
         start_response(status, header=[])
         return iter([response_body])
+
+
+app = API()
